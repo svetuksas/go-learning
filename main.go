988ab54761397f893378b8d5cb96e9383fc6b7e7
svetuksas <svetuksas@gmail.com>
2021-09-25 21:40:05 +0300

    input output operations with strings

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
+import "fmt"
+
 func main() {
-	print("_________\n|\t|\n|_______|\n")
-	print("    /\\\n   /  \\\n  /    \\\n  \\    / \n   \\  / \n    \\/") // <- type here
-	println()
+	// int -1,0,1,2,3
+	// float -1.1, 2.2,
+	// string "rabbit"
+	// rune
+	// uint 0,1,2,3,4
+
+	// var var_name type = value
+	// var a int = 3
+
+	// div - '/'
+	// mult - '*'
+	// sum - '+'
+	// dif - '-'
+	// var a float64 = 10.0
+	// var b float64 = 20.0
+	// fmt.Println(a + b)
+	// fmt.Println(a - b)
+	// fmt.Println(a * b)
+	// fmt.Println(a / b)
+
+	// fmt.Println(b / a)
+
+	// var a string // create variable
+	// fmt.Scanln(&a)
+	var a string
+	fmt.Scanln(&a)
+	var b string
+	fmt.Scanln(&b)
+
+	fmt.Println(a)
+	fmt.Println(b)
+
 }
